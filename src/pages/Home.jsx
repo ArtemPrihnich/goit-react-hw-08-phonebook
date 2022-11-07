2d138f9d4e9779e24e1d8c1539941c11973b0a79
ArtemPrihnich <artem.prihnich@gmail.com>
2022-11-07 19:41:07 +0200

    bug fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Box, color, Heading, Highlight } from '@chakra-ui/react'
+import { Box, Heading, Highlight } from '@chakra-ui/react'
 import React from 'react'
 
 export default function Home() {
